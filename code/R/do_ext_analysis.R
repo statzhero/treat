dddba2392fd433b704083cdbab5ffff1fb25c947
statzhero <panoramadata@gmail.com>
2021-05-07 13:49:51 -0400

    Add new files

---
+++ code/R/do_ext_analysis.R
@@ -0,0 +1,7 @@
+#' Extension for "Research on Corporate Transparency"
+#' Author: Ulrich Atz
+#' ---------------------------------------------------------
+
+library(tidyverse)
+
+smp <- readRDS("data/generated/acc_sample.rds")
